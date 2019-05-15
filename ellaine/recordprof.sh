231c6bc70f7ffea2a5121201e3f3aa620afe198a
Shanee Lu <shaneelu1@gmail.com>
2019-05-15 11:20:50 -0400

    removed bash script

--- ellaine/recordprof.sh
+++
@@ -1,1 +0,0 @@
-arecord --device=hw:1,0 --format S16_LE --rate 44100 --duration 5 -c1 prof.wav
--- ellaine/recordstud.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-arecord --device=hw:1,0 --format S16_LE --rate 44100 --duration 5 -c1 stud.wav
