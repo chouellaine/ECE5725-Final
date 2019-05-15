92b21e6b375c2c2dacdf8bc28914c602aa64698f
shaneelu <sl2363@cornell.edu>
2019-05-15 10:23:36 -0400

    Update home.py

--- ellaine/home.py
+++ ellaine/home.py
@@ -9,7 +9,9 @@ import os.path
 
 import os
 os.putenv('SDL_VIDEODRIVER','fbcon')
-os.putenv('SDL_FBDEV','/dev/fb0')
+os.putenv('SDL_FBDEV','/dev/fb1')
+os.putenv('SDL_MOUSEDRV', 'TSLIB')
+os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 
 import note
 import wave
