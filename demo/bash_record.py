43df2c97aadb43b8520b39f9ffe322d6eb8e5e91
shaneelu <sl2363@cornell.edu>
2019-05-17 19:23:45 -0400

    Update bash_record.py

--- demo/bash_record.py
+++ demo/bash_record.py
@@ -1,9 +1,11 @@
 import subprocess
 
+#Runs a bash script to record from the microphone. Stores as the student's recording.
 def record_stud():
     cmd = "arecord --device=hw:1,0 --format S16_LE --rate 44100 --duration 5 -c1 stud.wav"
     subprocess.check_output(cmd, shell=True)
-
+    
+#Runs a bash script to record from the microphone. Stores as the professional's recording.
 def record_prof():
     cmd = "arecord --device=hw:1,0 --format S16_LE --rate 44100 --duration 5 -c1 prof.wav"
     subprocess.check_output(cmd, shell=True)
